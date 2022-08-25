621891200a78a13c0ca05b86edfb1d8c984b6752
hybfkuf <fredjoseph863@gmail.com>
2022-08-25 17:52:32 +0800

    k8s-loadbalancer.go

--- k8s-loadbalancer.go
+++ k8s-loadbalancer.go
@@ -3,15 +3,13 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
 	"net"
 
-	"github.com/forbearing/k8s"
+	"github.com/forbearing/k8s-loadbalancer/pkg/controller"
 	"github.com/forbearing/k8s/service"
 	"github.com/forbearing/k8s/util/signals"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type updateObj struct {
@@ -32,49 +30,51 @@ func main() {
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
 
-	clientset := k8s.ClientsetOrDie(*argKubeConfigFile)
-	stopCh := signals.SetupSignalChannel()
-	_ = clientset
-	_ = stopCh
-
 	handler := service.NewOrDie(context.Background(), *argKubeConfigFile, "")
+	stopCh := signals.SetupSignalChannel()
+	ctrl := controller.NewController(handler.Clientset(), handler.ServiceInformer())
 
-	addQueue := make(chan interface{}, 100)
-	updateQueue := make(chan updateObj, 100)
-	deleteQueue := make(chan interface{}, 100)
-
-	addFunc := func(obj interface{}) { addQueue <- obj }
-	updateFunc := func(oldObj interface{}, newObj interface{}) {
-		uo := updateObj{}
-		uo.oldObj = oldObj
-		uo.newObj = newObj
-		updateQueue <- uo
+	handler.InformerFactory().Start(stopCh)
+	if err := ctrl.Run(2, stopCh); err != nil {
+		logrus.Fatal("Err running controller: %s", err.Error())
 	}
-	deleteFunc := func(obj interface{}) { deleteQueue <- obj }
 
-	go func() {
-		handler.RunInformer(stopCh, addFunc, updateFunc, deleteFunc)
-	}()
+	//addQueue := make(chan interface{}, 100)
+	//updateQueue := make(chan updateObj, 100)
+	//deleteQueue := make(chan interface{}, 100)
 
-	for {
-		select {
-		case obj := <-addQueue:
-			myObj := obj.(metav1.Object)
-			log.Println("Add: ", myObj.GetName())
-		case ou := <-updateQueue:
-			oldObj := ou.oldObj.(*corev1.Service)
-			curObj := ou.newObj.(*corev1.Service)
-			if oldObj.ResourceVersion == curObj.ResourceVersion {
-				return
-			}
-			log.Println("Update:", curObj.Name)
-		case obj := <-deleteQueue:
-			myObj := obj.(metav1.Object)
-			log.Println("Delete", myObj.GetName())
-		case <-stopCh:
-			log.Println("Informer stopped.")
-			return
-		}
+	//addFunc := func(obj interface{}) { addQueue <- obj }
+	//updateFunc := func(oldObj interface{}, newObj interface{}) {
+	//    uo := updateObj{}
+	//    uo.oldObj = oldObj
+	//    uo.newObj = newObj
+	//    updateQueue <- uo
+	//}
+	//deleteFunc := func(obj interface{}) { deleteQueue <- obj }
 
-	}
+	//go func() {
+	//    handler.RunInformer(stopCh, addFunc, updateFunc, deleteFunc)
+	//}()
+
+	//for {
+	//    select {
+	//    case obj := <-addQueue:
+	//        myObj := obj.(metav1.Object)
+	//        log.Println("Add: ", myObj.GetName())
+	//    case ou := <-updateQueue:
+	//        oldObj := ou.oldObj.(*corev1.Service)
+	//        curObj := ou.newObj.(*corev1.Service)
+	//        if oldObj.ResourceVersion == curObj.ResourceVersion {
+	//            return
+	//        }
+	//        log.Println("Update:", curObj.Name)
+	//    case obj := <-deleteQueue:
+	//        myObj := obj.(metav1.Object)
+	//        log.Println("Delete", myObj.GetName())
+	//    case <-stopCh:
+	//        log.Println("Informer stopped.")
+	//        return
+	//    }
+
+	//}
 }
