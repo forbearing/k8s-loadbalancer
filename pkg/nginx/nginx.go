a494428b08cb09c711cf1a1e65796ed04a05495a
hybfkuf <fredjoseph863@gmail.com>
2022-08-26 11:10:12 +0800

    Update nginx package

--- pkg/nginx/nginx.go
+++ pkg/nginx/nginx.go
@@ -1,129 +1,139 @@
 package nginx
 
 import (
+	"bytes"
+	"errors"
 	"io"
 	"os/exec"
+	"strconv"
 	"sync"
 
 	"github.com/sirupsen/logrus"
 )
 
 var (
-	stdout = logrus.New().Writer()
-	stderr = logrus.New().WriterLevel(logrus.ErrorLevel)
-)
-var (
-	lock sync.Mutex
+	locker sync.RWMutex
+
+	stdout = logrus.New().WriterLevel(logrus.DebugLevel)
 )
 
-// Install will intall the nginx package in linux.
-func Install() error {
-	lock.Lock()
-	defer lock.Unlock()
-
-	return executeCommand([]string{"bash", "-c", `
-source /etc/os-release
-linuxID=$ID
-linuxMajorVersion=$( echo $VERSION | awk -F'[.| ]' '{print $1}' )
-linuxCodeName=$VERSION_CODENAME
-[ -f /etc/lsb-release ] &&  \
-	linuxMinorVersion=$(cat /etc/lsb-release  | awk -F'=' '/DISTRIB_RELEASE/ {print $2}' | awk -F'.'  '{print $2}')
-[ -f /etc/system-release ] && \
-	linuxMinorVersion=$(cat /etc/system-release | awk '{print $4}' | awk -F'.' '{print $2}')
-
-case $linuxID in 
-ubuntu|debian)
-	if ! command -v nginx &> /dev/null; then
-		apt-get update
-		apt-get install -y nginx
-	fi; ;;
-centos|rocky)
-	if ! command -v nginx &> /dev/null; then
-		yum install -y nginx
-	fi; ;;
-esac
-
-	`}, stdout, stderr)
+type Nginx struct {
+	err error
 }
 
-// Uninstall() will uninstall the nginx package in linux.
-func Uninstall() error {
-	lock.Lock()
-	defer lock.Unlock()
+// Err returns the first errors that was encountered by the Do() method.
+func (n *Nginx) Err() error {
+	return n.err
+}
 
-	return executeCommand([]string{"bash", "-c", `
-source /etc/os-release
-linuxID=$ID
-linuxMajorVersion=$( echo $VERSION | awk -F'[.| ]' '{print $1}' )
-linuxCodeName=$VERSION_CODENAME
-[ -f /etc/lsb-release ] &&  \
-	linuxMinorVersion=$(cat /etc/lsb-release  | awk -F'=' '/DISTRIB_RELEASE/ {print $2}' | awk -F'.'  '{print $2}')
-[ -f /etc/system-release ] && \
-	linuxMinorVersion=$(cat /etc/system-release | awk '{print $4}' | awk -F'.' '{print $2}')
+// setErr records the first error encountered by the Do() method.
+func (n *Nginx) setErr(err error) {
+	if n.err == nil {
+		n.err = err
+	}
+}
 
-case $linuxID in 
-ubuntu|debian)
-	if command -v nginx &> /dev/null; then
-		systemctl disable --now nginx &> /dev/null
-		apt-get purge -y nginx*
-	fi; ;;
-centos|rocky)
-	if command -v nginx &> /dev/null; then
-		systemctl disable --now nginx &> /dev/null
-		yum remove -y nginx*
-	fi; ;;
-esac
-	`}, stdout, stderr)
+// Install will intall the nginx package in linux.
+func Install() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", NGINX_INSTALL}, stdout, errBuf)
+}
+
+// Remove() will uninstall the nginx package in linux.
+func Remove() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", NGINX_REMOVE}, stdout, errBuf)
 }
 
 // Start will start nginx daemon by systemctl.
-func Start() error {
-	return executeCommand([]string{"systemctl", "start", "nginx"}, stdout, stderr)
+func Start() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", NGINX_START}, stdout, errBuf)
 }
 
 // Stop will stop nginx daemon by systemctl.
-func Stop() error {
-	return executeCommand([]string{"systemctl", "stop", "nginx"}, stdout, stderr)
+func Stop() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", NGINX_STOP}, stdout, errBuf)
 }
 
 // Restart will restart nginx daemon by systemctl.
-func Restart() error {
-	return executeCommand([]string{"systemctl", "restart", "nginx"}, stdout, stderr)
+func Restart() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", NGINX_RESTART}, stdout, errBuf)
 }
 
 // Reload will reload nginx daemon by systemctl.
-func Reload() error {
-	return executeCommand([]string{"systemctl", "reload", "nginx"}, stdout, stderr)
+func Reload() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", NGINX_RELOAD}, stdout, errBuf)
 }
 
 // EnabledNow will enabled and start nginx daemon by systemctl.
-func EnabledNow() error {
-	return executeCommand([]string{"systemctl", "enabled", "--now", "nginx"}, stdout, stderr)
+func EnabledNow() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", NGINX_ENABLEDNOW}, stdout, errBuf)
 }
 
 // Enabled will enabled nginx daemon by systemctl.
-func Enabled() error {
-	return executeCommand([]string{"systemctl", "enabled", "nginx"}, stdout, stderr)
+func Enabled() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", NGINX_ENABLED}, stdout, errBuf)
 }
 
 // TestConf will test nginx configuration file.
-func TestConf() error {
-	return executeCommand([]string{"nginx", "-t"}, stdout, stderr)
+func TestConf() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", NGINX_TESTCONF}, stdout, errBuf)
+}
+func Version() (error, int, string) {
+	locker.Lock()
+	defer locker.Unlock()
+	errBuf := &bytes.Buffer{}
+	return executeCommand([]string{"bash", "-c", "nginx version"}, stdout, errBuf)
 }
 
 // executeCommand execute linux command.
-func executeCommand(command []string, stdout, stderr io.Writer) error {
-	if _, err := exec.LookPath(command[0]); err != nil {
-		return err
-	}
+// if command exit code is 0, ignore command stderr output.
+func executeCommand(command []string, stdout io.Writer, errBuf *bytes.Buffer) (error, int, string) {
 	cmd := exec.Command(command[0], command[1:]...)
 	cmd.Stdout = stdout
-	cmd.Stderr = stderr
-	if err := cmd.Start(); err != nil {
-		return err
+	cmd.Stderr = errBuf
+	err := cmd.Run()
+	if err != nil {
+		if msg, ok := err.(*exec.ExitError); ok {
+			return err, msg.ExitCode(), errBuf.String()
+		}
+		return err, 0, errBuf.String()
 	}
-	if err := cmd.Wait(); err != nil {
-		return err
+	return nil, 0, errBuf.String()
+}
+
+// CmdErrMsg returns the exec.Command error and command stderr output.
+func CmdErrMsg(err error, exitCode int, errMsg string) error {
+	if err != nil {
+		return errors.New(err.Error() + ": " + errMsg)
+	}
+	if exitCode != 0 {
+		return errors.New("exit status " + strconv.Itoa(exitCode) + ": " + errMsg)
 	}
 	return nil
 }
